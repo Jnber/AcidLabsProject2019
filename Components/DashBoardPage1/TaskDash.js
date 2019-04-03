1a7c832be5d4b67ad002453078f21fc9b8c013e7
Jnber <46068241+Jnber@users.noreply.github.com>
2019-04-03 21:23:32 +0200

    Update TaskDash.js

--- Components/DashBoardPage1/TaskDash.js
+++ Components/DashBoardPage1/TaskDash.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import {CheckBox , Text, View, Button} from 'react-native';
-import DashClient from './TaskDash/DashClient';
-import DashTime from './TaskDash/DashTime';
-import DashTypeTask from './TaskDash/DashTypeTask';
-import DashAcceptButton from './TaskDash/DashAcceptButton';
+import {  Text, View, Image , Button  } from 'react-native';
 
-export default class DashBoard extends  React.Component{
-    constructor(props) {
-        super(props);
-       this.state={
-       }
-    }
-    
+
+class TaskDash extends React.Component {
+   state={
+     num:1,
+     time:'13:00',
+     cate:'Uber'
+   }
+  render() {
+      return (
+      <View style={{flexDirection: 'row'}}>
+        <View style={{flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around'}}>
+          <Text >#Client {this.state.num} </Text>
+          <Text/><Text/><Text/><Text/>
+          <Button title="Accept" />
+        </View>
+        <View>
+          <Text>{this.state.time}</Text>
+          <Text/><Text/><Text/>
+          <Text>{this.state.cate}</Text>
+          <Button title='Fournisseur'/>
+        </View>
+      </View>
+      );
    
-    
-    render() {
-      
-        return (
-            <View >
-            <DashClient/>
-            <DashTime/>
-            <DashTypeTask/>
-            <DashAcceptButton/>
-            </View>
-            );
-    }
-
-} 
+  }
+}
