acd5e2f7ad12c7c8b7ded9a36d4517e304dece88
Jnber <46068241+Jnber@users.noreply.github.com>
2019-04-03 21:27:04 +0200

    Update DashBoardScreen.js

--- Components/DashBoardPage1/DashBoardScreen.js
+++ Components/DashBoardPage1/DashBoardScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {CheckBox ,StyleSheet, Text, View, Button} from 'react-native';
 import { Container, Header, Body, Right, Title, Content, Form, Item, Input, Label,Left, Icon} from 'native-base';
 import DashHeader1 from './DashHeader1';
+import TaskDash from './TaskDash';
 
 export default class DashBoardapp extends  React.Component{
     
@@ -10,6 +11,11 @@ export default class DashBoardapp extends  React.Component{
         return (
             <View style={styles.container}>
            <DashHeader1 navigation={this.props.navigation}/>
+            <TaskDash/>
+            <Text/>
+            <TaskDash/>
+            <Text/>
+            <TaskDash/>
             </View>
             );
     }
